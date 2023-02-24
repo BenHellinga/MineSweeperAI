75fdedc18cc6d7903bbdb63cc11a08476a038c29
BenHellinga <benhellinga@outlook.com>
2023-02-24 16:27:27 -0500

    Add project files.

---
+++ MineSweeper/GamePlayer.cs
@@ -0,0 +1,323 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using HumanBenchmark;
+using System.Windows.Forms;
+using System.Threading;
+using System.Collections;
+using Microsoft.Win32.SafeHandles;
+
+namespace MineSweeper
+{
+    internal class GamePlayer
+    {
+        // CONSTANTS
+
+        public const int SCAN_SIZE = 30;
+
+        public const int GAME_WIDTH = 30;
+        public const int GAME_HEIGHT = 16;
+
+        public const int DELAY_AFTER_CLICK = 100;
+        public const int CLICK_DELAY = 0;
+        public const int SCREENSHOT_DELAY = 50;
+
+        public const int BUFFER_SIZE = GAME_WIDTH * GAME_HEIGHT / 2;
+
+        public const int CLICK_OFFSET_X = 10;
+        public const int CLICK_OFFSET_Y = 10;
+
+        public const int RANDOM_MIN_EXPLORED = 2;
+        public const bool PAUSE_BEFORE_RANDOM = false;
+
+        // VARIABLES
+
+
+        public static ScreenReader reader;
+
+        public static Point gameLocation;
+
+        public static Tile[,] board;
+
+        //public static Tile[] explore = new Tile[BUFFER_SIZE];
+        public static Tile[] read = new Tile[BUFFER_SIZE];
+        public static Tile[] update = new Tile[BUFFER_SIZE];
+        public static List<Tile> active = new List<Tile>();
+
+        //public static int exploreNum;
+        public static int readNum;
+        public static int updateNum;
+
+        public static bool change;
+
+
+        // MAIN
+
+        static void Main()
+        {
+
+            initBoard();
+
+            gameLocation = CursorManager.findColorNearCursor(Tile.tileColor, SCAN_SIZE, SCAN_SIZE);
+
+            reader = new ScreenReader(gameLocation.X, gameLocation.Y, 100, 100);
+            reader.captureScreenshot();
+            findTileSize();
+
+            reader = new ScreenReader(gameLocation.X, gameLocation.Y, GAME_WIDTH * Tile.size, GAME_HEIGHT * Tile.size);
+            reader.captureScreenshot();
+
+            CursorManager.click(gameLocation.X + Tile.size * GAME_WIDTH / 2 + GAME_WIDTH / 2,
+                                    gameLocation.Y + Tile.size * GAME_HEIGHT / 2 + GAME_HEIGHT / 2, "left");
+            Thread.Sleep(DELAY_AFTER_CLICK);
+
+            read[0] = board[GAME_WIDTH / 2, GAME_HEIGHT / 2];
+            read[0].status = 1;
+
+            readNum = 0;
+            updateNum = -1;
+
+            int j = 400;
+            while (j > 0)
+            {
+                if (CursorManager.cursorMoved())
+                {
+                    Console.WriteLine("Paused");
+                    Console.ReadLine();
+                }
+
+                readNewTiles();
+                updateNewTiles();
+
+                if (active.Count == 0) break;
+
+                change = false;
+                flagTrivialTiles(ref change);
+                finishMinelessTiles(ref change);
+
+                if (!change)
+                    considerOverlap(ref change);
+
+                if (!change)
+                    pickLowestProbabilty(ref change);
+
+                removeFinishedTiles();
+
+                if (!change) break;
+                j--;
+            }
+
+            /*
+            Console.WriteLine("ACTIVE");
+            for (int i = 0; i < active.Count; i++)
+                active[i].print();
+            Console.WriteLine("READ");
+            for (int i = 0; i <= readNum; i++)
+                read[i].print();
+            Console.WriteLine("UPDATE");
+            for (int i = 0; i <= updateNum; i++)
+                update[i].print();
+            */
+
+            Console.WriteLine("Finished");
+            Console.ReadLine();
+        }
+
+
+
+        // PUBLIC METHODS
+
+        public static void initBoard()
+        {
+            Tile.init();
+            board = new Tile[GAME_WIDTH, GAME_HEIGHT];
+
+            for (int x = 0; x < GAME_WIDTH; x++)
+                for (int y = 0; y < GAME_HEIGHT; y++)
+                    board[x, y] = new Tile(x, y);
+
+            for (int x = 0; x < GAME_WIDTH; x++)
+                for (int y = 0; y < GAME_HEIGHT; y++)
+                    board[x, y].initUnexploredAdj();
+        }
+
+
+        public static void findTileSize()
+        {
+            Tile.size = 1;
+            while (reader.screenshot.GetPixel(Tile.size, 10).Equals(Tile.tileColor)) Tile.size++;
+            while (!reader.screenshot.GetPixel(Tile.size, 10).Equals(Tile.tileColor)) Tile.size++;
+        }
+
+
+
+        public static void print()
+        {
+            for (int y = 0; y < GAME_HEIGHT; y++)
+            {
+                for (int x = 0; x < GAME_WIDTH; x++)
+                {
+                    Console.Write((board[x, y].unexploredNum) + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+
+
+        public static void readNewTiles()
+        {
+            Thread.Sleep(SCREENSHOT_DELAY);
+            reader.captureScreenshot();
+
+            Tile tile;
+            while (readNum >= 0)
+            {
+                tile = read[readNum];
+                readNum--;
+                tile.read();
+            }
+        }
+
+
+
+        public static void updateNewTiles()
+        {
+            Tile tile;
+            while (updateNum >= 0)
+            {
+                tile = update[updateNum];
+                updateNum--;
+                tile.update();
+
+                active.Add(tile);
+            }
+        }
+
+
+
+        public static void finishMinelessTiles(ref bool change)
+        {
+            for (int i = 0; i < active.Count; i++)
+            {
+                if (active[i].minesLeft == 0)
+                {
+                    active[i].exploreAdjacent();
+                    change = true;
+                }
+            }
+        }
+
+
+
+        public static void flagTrivialTiles(ref bool change)
+        {
+            for (int i = 0; i < active.Count; i++)
+            {
+                if (active[i].minesLeft == active[i].unexploredNum)
+                {
+                    active[i].flagAdjacent();
+                    change = true;
+                }
+            }
+        }
+
+
+
+        public static void considerOverlap(ref bool change)
+        {
+            for (int i = 0; i < active.Count; i++)
+            {
+                active[i].considerSharedMines(ref change);
+            }
+        }
+
+
+
+        public static void pickLowestProbabilty(ref bool change)
+        {
+            Tile tile;
+            Tile best = board[0, 0];
+
+            int count;
+            double sumProb = 0;
+            double min = 1;
+
+
+            for (int y = 0; y < GAME_HEIGHT; y++)
+            {
+                for (int x = 0; x < GAME_WIDTH; x++)
+                {
+                    if (board[x, y].status != 0) continue;
+
+                    count = 0;
+                    for (int i = 0; i < 8; i++)
+                    {
+                        tile = board[x, y].adjacentTiles[i];
+
+                        if (tile.status == 3)
+                        {
+                            sumProb += tile.minesLeft / (double)tile.unexploredNum;
+                            count++;
+                        }
+
+                        //if (tile.status == 6) count++;
+
+                    }
+
+                    if (count < RANDOM_MIN_EXPLORED) continue;
+
+                    sumProb /= count;
+
+                    if (sumProb < min)
+                    {
+                        min = sumProb;
+                        best = board[x, y];
+                    }
+
+                }
+            }
+
+            if (min == 1) return;
+
+            Console.WriteLine("random guess -> x: " + best.x + "   y: " + best.y + " p: ~" + min + "%");
+            if (PAUSE_BEFORE_RANDOM) Console.ReadLine();
+
+            best.exploreTile();
+            change = true;
+
+        }
+
+
+
+        public static void removeFinishedTiles()
+        {
+            for (int i = 0; i < active.Count; i++)
+            {
+                if (active[i].status == 4)
+                {
+                    //Console.WriteLine("removing " + active[i].x + " " + active[i].y);
+                    active.RemoveAt(i);
+                    i--;
+                }
+            }
+        }
+
+
+
+        public static void markActiveTiles()
+        {
+            for (int i = 0; i < active.Count; i++)
+                for (int j = 0; j < 10; j++)
+                    for (int k = 0; k < 10; k++)
+                        reader.screenshot.SetPixel(active[i].x * Tile.size + j, active[i].y * Tile.size + k, Color.FromArgb(255, 0, 0, 0));
+        }
+
+
+        
+
+    }
+}
---
+++ MineSweeper/Tile.cs
@@ -0,0 +1,398 @@
+﻿using HumanBenchmark;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Policy;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MineSweeper
+{
+    internal class Tile
+    {
+
+        // VARIABLES
+
+        // static
+        
+        public static int size;
+
+        public static Color tileColor = Color.FromArgb(255, 255, 255, 255);
+        public static Color emptyColor = Color.FromArgb(255, 189, 189, 189);
+        public static Color flagColor = Color.FromArgb(255, 255, 0, 0);
+        public static List<Color> numberColors = new List<Color>()
+            { Color.FromArgb(255, 0, 0, 255),
+              Color.FromArgb(255, 0, 123, 0),
+              Color.FromArgb(255, 255, 0, 0),
+              Color.FromArgb(255, 0, 0, 123),
+              Color.FromArgb(255, 123, 0, 0),
+              Color.FromArgb(255, 0, 123, 123),
+              Color.FromArgb(255, 0, 0, 0),
+              Color.FromArgb(255, 123, 123, 123)};
+
+
+        // not static
+
+        public int x;
+        public int y;
+
+        public int num;
+
+        public int minesLeft;
+        public int unexploredNum;
+
+        public Tile[] adjacentTiles = new Tile[8];
+
+        public int status;
+
+        public static Tile NULL;
+
+
+
+        // CONSTRUCTORS
+
+        public Tile(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+
+            num = -1;
+            minesLeft = -1;
+            status = 0;
+        }
+
+
+
+        // PUBLIC METHODS
+
+        public void read()
+        {
+            num = readNumber();
+            minesLeft = num;
+
+            if (num == 0)
+            {
+                Tile tile;
+
+                for (int i = 0; i < 8; i++)
+                {
+                    tile = adjacentTiles[i];
+
+                    tile.unexploredNum--;
+
+                    if (tile.status == 1) continue;
+
+                    if (tile.status == 0)
+                    {
+                        tile.status = 1;
+                        tile.addTileToRead();
+                    }
+                }
+
+                unexploredNum = 0;
+                return;
+            }
+
+            if (num > 0)
+            {
+                if (status == 1)
+                {
+                    GamePlayer.updateNum++;
+                    GamePlayer.update[GamePlayer.updateNum] = this;
+                    status = 2;
+                }
+
+                return;
+            }
+
+            if (num == -2)
+            {
+                Tile tile;
+
+                status = 6;
+
+                for (int i = 0; i < 8; i++)
+                {
+                    tile = adjacentTiles[i];
+
+                    if (tile.status == 3)
+                    {
+                        tile.minesLeft--;
+                        tile.unexploredNum--;
+
+                        if (tile.unexploredNum == 0)
+                            tile.status = 4;
+                    }
+                }
+
+                return;
+            }
+
+        }
+
+
+        
+        public void update()
+        {
+            Tile tile;
+
+            status = 3;
+
+            for (int i = 0; i < 8; i++)
+            {
+                tile = adjacentTiles[i];
+
+                if (tile.status == 5 || tile.status == 6)
+                {
+                    minesLeft--;
+                    unexploredNum--;
+                    continue;
+                }
+
+                if (tile.status == 3 || tile.status == 4)
+                {
+                    tile.unexploredNum--;
+                    unexploredNum--;
+                }
+            }
+        }
+
+
+
+        public void exploreAdjacent()
+        {
+            status = 4;
+
+            //Console.WriteLine("middle clicking");
+            CursorManager.click(x * Tile.size + GamePlayer.gameLocation.X + GamePlayer.CLICK_OFFSET_X,
+                                y * Tile.size + GamePlayer.gameLocation.Y + GamePlayer.CLICK_OFFSET_Y, "middle");
+            Thread.Sleep(GamePlayer.CLICK_DELAY);
+
+            Tile tile;
+
+            for (int i = 0; i < 8; i++)
+            {
+                tile = adjacentTiles[i];
+
+                if (tile.status == 0)
+                {
+                    tile.status = 1;
+                    tile.addTileToRead();
+                }
+            }
+        }
+
+
+
+        public void flagAdjacent()
+        {
+            status = 4;
+
+            Tile tile;
+
+            for (int i = 0; i < 8; i++)
+            {
+                tile = adjacentTiles[i];
+
+                if (tile.status == 0)
+                    tile.flagTile();
+            }
+        }
+
+
+
+        public void considerSharedMines(ref bool change)
+        {
+            Tile tileA;
+            Tile tileB;
+
+            int count;
+
+            int oxmin;
+            int oxmax;
+            int oymin;
+            int oymax;
+
+            for (int k = 0; k < 8; k++)
+            {
+                tileB = adjacentTiles[k];
+
+                if (tileB.status != 3) continue;
+                if (tileB.minesLeft != 1) continue;
+
+                oxmin = Math.Max(x - 1, tileB.x - 1);
+                oxmax = Math.Min(x + 1, tileB.x + 1);
+                oymin = Math.Max(y - 1, tileB.y - 1);
+                oymax = Math.Min(y + 1, tileB.y + 1);
+
+                count = 0;
+                for (int i = -1; i <= 1; i++)
+                {
+                    for (int j = -1; j <= 1; j++)
+                    {
+                        if (x + j >= oxmin && x + j <= oxmax &&
+                            y + i >= oymin && y + i <= oymax)
+                            continue;
+
+                        if (i == 0 && j == 0) continue;
+
+                        if (x + j < 0 || x + j >= GamePlayer.GAME_WIDTH ||
+                            y + i < 0 || y + i >= GamePlayer.GAME_HEIGHT)
+                            continue;
+
+                        if (GamePlayer.board[x + j, y + i].status == 0 ||
+                            GamePlayer.board[x + j, y + i].status == 5)
+                            count++;
+                    }
+                }
+
+                //Console.WriteLine(count + " " + minesLeft + " " + tileB.minesLeft);
+                if (count > minesLeft - tileB.minesLeft) continue;
+
+                for (int i = -1; i <= 1; i++)
+                {
+                    for (int j = -1; j <= 1; j++)
+                    {
+                        if (x + j >= oxmin && x + j <= oxmax &&
+                            y + i >= oymin && y + i <= oymax)
+                            continue;
+
+                        if (i == 0 && j == 0) continue;
+
+                        tileA = adjacentTiles[(j + 1) + 3 * (i + 1) -
+                                             ((j + 1) + 3 * (i + 1) > 4 ? 1 : 0)]; // subtract 1 cuz it skips over .this
+
+                        if (tileA.status == 0)
+                        {
+                            tileA.flagTile();
+                            change = true;
+                        }
+                    }
+                }
+
+                for (int i = -1; i <= 1; i++)
+                {
+                    for (int j = -1; j <= 1; j++)
+                    {
+                        if (tileB.x + j >= oxmin && tileB.x + j <= oxmax &&
+                            tileB.y + i >= oymin && tileB.y + i <= oymax)
+                            continue;
+
+                        if (i == 0 && j == 0) continue;
+
+                        tileA = tileB.adjacentTiles[(j + 1) + 3 * (i + 1) -
+                                                   ((j + 1) + 3 * (i + 1) > 4 ? 1 : 0)]; // subtract 1 cuz it skips over .this
+
+                        if (tileA.status == 0)
+                        {
+                            tileA.exploreTile();
+                            change = true;
+                        }
+                    }
+                }
+                //Console.WriteLine();
+            }
+        }
+
+        public void print()
+        {
+            Console.Write("   x: " + x + "   y: " + y);
+            Console.Write("   number: " + num + "   unexplored: " + unexploredNum);
+            Console.WriteLine("   mines left: " + minesLeft + "   status: " + status);
+        }
+        
+
+
+        public int readNumber()
+        {
+            if (GamePlayer.reader.screenshot.GetPixel(x * size, y * size).Equals(tileColor))
+            {
+                for (int i = size / 3; i < 2 * size / 3; i++)
+                    for (int j = 0; j < 8; j++)
+                        if (GamePlayer.reader.screenshot.GetPixel(x * size + i, y * size + i).Equals(flagColor))
+                            return -2;
+
+                return -1;
+            }
+
+            for (int i = size / 4; i < 3 * size / 4; i++)
+                for (int j = 0; j < 8; j++)
+                    if (GamePlayer.reader.screenshot.GetPixel(x * size + i, y * size + i).Equals(numberColors[j]))
+                        return j + 1;
+
+            return 0;
+        }
+
+
+
+        public void initUnexploredAdj()
+        {
+            int k = -1;
+            unexploredNum = 0;
+            for (int i = -1; i <= 1; i++)
+            {
+                for (int j = -1; j <= 1; j++)
+                {
+                    if (i == 0 && j == 0) continue;
+
+                    k++;
+
+                    if (x + j < 0 || x + j >= GamePlayer.GAME_WIDTH ||
+                        y + i < 0 || y + i >= GamePlayer.GAME_HEIGHT)
+                    {
+                        adjacentTiles[k] = NULL;
+                        continue;
+                    }
+
+                    unexploredNum++;
+                    adjacentTiles[k] = GamePlayer.board[x + j, y + i];
+                }
+            }
+        }
+
+
+
+        public static void init()
+        {
+            NULL = new Tile(0, 0);
+            NULL.status = -1;
+        }
+
+
+
+        public void addTileToRead()
+        {
+            GamePlayer.readNum++;
+            GamePlayer.read[GamePlayer.readNum] = this;
+        }
+
+
+
+        public void exploreTile()
+        {
+            CursorManager.click(x * Tile.size + GamePlayer.gameLocation.X + GamePlayer.CLICK_OFFSET_X,
+                                y * Tile.size + GamePlayer.gameLocation.Y + GamePlayer.CLICK_OFFSET_Y, "left");
+            Thread.Sleep(GamePlayer.CLICK_DELAY);
+
+            status = 1;
+            addTileToRead();
+        }
+
+
+
+        public void flagTile()
+        {
+            CursorManager.click(x * Tile.size + GamePlayer.gameLocation.X + GamePlayer.CLICK_OFFSET_X,
+                                y * Tile.size + GamePlayer.gameLocation.Y + GamePlayer.CLICK_OFFSET_Y, "right");
+            Thread.Sleep(GamePlayer.CLICK_DELAY);
+
+            status = 5;
+            addTileToRead();
+        }
+    }
+}
